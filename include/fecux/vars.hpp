dfd22d3dcba10717de740a253e942c19169bc56a
Linuxperoxo <linuxperoxo@gmail.com>
2024-09-03 20:27:24 -0300

    update FH

--- include/fecux/vars.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef __VARS_HPP__
-#define __VARS_HPP__
-
-constexpr const char* CONFIG_FILE = "/etc/fecux.conf";
-
-#endif
--- src/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "../include/fecux/config.hpp"
-
-int main(){
-  fecux::main::config _config;
-  return 0;
-}
