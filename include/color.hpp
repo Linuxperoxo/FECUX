f2668f709a8cb04471a4f776967c0caf8eeacd5b
Linuxperoxo <linuxperoxo@gmail.com>
2024-09-07 21:05:42 -0300

    update all

--- include/color.hpp
+++ include/color.hpp
@@ -7,6 +7,19 @@
 #ifndef COLOR_HPP
 #define COLOR_HPP
 
+/*
+
+  Esse arquivo só define algumas cores
+  para deixar o out bonitinho >:)
+  
+  Você pode alterar(O VALOR) se quiser.
+  
+  Não modifique o nome da variável, senão
+  você vai ter que mudar em todas as partes
+  do código
+  
+ */
+
 constexpr const char* RED = "\033[031m";
 constexpr const char* GREEN = "\033[032m";
 constexpr const char* YELLOW = "\033[033m";
--- include/fecux_vars.hpp
+++ include/fecux_vars.hpp
@@ -7,6 +7,13 @@
 #ifndef __FECUX_VARS_H__
 #define __FECUX_VARS_H__
 
+/*
+
+  Algumas vars globais para o gerenciador.
+  Só altere se você quiser uma melhor organização
+  
+ */
+
 constexpr const char* CONFIG_FILE   = "/etc/fecux.conf";
 constexpr const char* MAIN_REPO_DIR = "/var/db/fecux/repos/main";
 constexpr const char* WORLD_DIR     = "/var/db/fecux/pkgs/world";
