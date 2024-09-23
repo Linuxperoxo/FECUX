7b3c459fb44dae813f5a03db1989992867481973
Linuxperoxo <167115676+Linuxperoxo@users.noreply.github.com>
2024-09-23 18:19:38 -0300

    Add files via upload

--- include/color.hpp
+++ include/color.hpp
@@ -9,10 +9,10 @@
 #ifndef __COLOR_HPP__
 #define __COLOR_HPP__
 
-constexpr const char *RED{"\033[031m"};
-constexpr const char *GREEN{"\033[032m"};
-constexpr const char *YELLOW{"\033[033m"};
-constexpr const char *BLUE{"\033[034;2m"};
-constexpr const char *NC{"\033[0m"};
+constexpr const char* RED{"\033[031m"};
+constexpr const char* GREEN{"\033[032m"};
+constexpr const char* YELLOW{"\033[033m"};
+constexpr const char* BLUE{"\033[034;2m"};
+constexpr const char* NC{"\033[0m"};
 
 #endif
---
+++ include/globals.hpp
@@ -0,0 +1,66 @@
+/*
+
+  COPYRIGHT: (c) 2024 per Linuxperoxo.
+  AUTHOR: Linuxperoxo
+  FILE: globals.hpp
+
+*/
+
+#ifndef __GLOBALS_HPP__
+#define __GLOBALS_HPP__
+
+/*
+
+  @info: Esse arquivo guarda variáveis gerais para o funcionamento
+  do fecux. Você pode alterar se quiser, mas o funcionamento não
+  é garantido, teste e veja o resultado você mesmo =)
+
+  OBS: Caso o projeto seja compilado e depois haja uma modificação
+  nesse arquivo, vocẽ terá que recompilar o projeto
+
+*/
+
+/*
+
+CONFIG_FILE_LOCALE:
+
+  * Variável para o arquivo de configuraç~ao do fecux
+
+  * Ela pode ser modificada sem preocupações
+
+*/
+
+constexpr const char* CONFIG_FILE_LOCALE{"/etc/fecux.conf"};
+
+/*
+
+  REPO_DIR:
+    
+    * Variável para o diretório do repositório do gerenciador
+    
+    * Não recomendo a modificação de tal, pode ser problemático 
+    já que não houve nenhum teste
+
+*/
+
+
+constexpr const char* REPO_DIR{"/var/db/fecux/repo/"};
+
+/*
+
+  WORLD_DIR:
+    
+    * Variável para o diretório dos pacotes instalados
+    
+    * Não existe uma necessidade real em mudar esse local, 
+    esse diretório vai armazenar apenas simples arquivos de texto
+
+    OBS: DELETAR ESSE DIRETÓRIO USANDO RM -RF FARÁ COM QUE TODOS OS PACOTES
+    INSTALADOS FIQUEM OBSOLETOS NO SISTEMA
+
+*/
+
+
+constexpr const char* WORLD_DIR{"/var/db/fecux/world/"};
+
+#endif
